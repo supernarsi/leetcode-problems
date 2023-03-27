6db9646f67eba8ebb27e0fa7e8b94c281d1f2c43
Narsi <narsihuang@gmail.com>
2023-03-27 19:58:11 +0800

    add answer

--- problems/1638.统计只差一个字符的子串数目.go
+++ problems/1638.统计只差一个字符的子串数目.go
@@ -7,8 +7,24 @@ package problems
  */
 
 // @lc code=start
-func countSubstrings(s string, t string) int {
-	return 0
+func countSubstrings(s, t string) (ans int) {
+	m, n := len(s), len(t)
+	for i := 0; i < m; i++ {
+		for j := 0; j < n; j++ {
+			diff := 0
+			for k := 0; i+k < m && j+k < n; k++ {
+				if s[i+k] != t[j+k] {
+					diff++
+				}
+				if diff > 1 {
+					break
+				} else if diff == 1 {
+					ans++
+				}
+			}
+		}
+	}
+	return ans
 }
 
 // @lc code=end
